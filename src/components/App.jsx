69bcbd67e99eb0051a855b932dce87d3ed22053e
Josh Womack <womack.sjosh@gmail.com>
2018-10-05 06:53:28 -0500

    Added card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,10 @@
 import React, { Component } from 'react';
-import Table from './Table';
 
-class App extends Component {
-    render() {
-        const characters = [
-            {
-                'name': 'Charlie',
-                'job': 'Janitor'
-            },
-            {
-                'name': 'Mac',
-                'job': 'Bouncer'
-            },
-            {
-                'name': 'Dee',
-                'job': 'Aspring actress'
-            },
-            {
-                'name': 'Dennis',
-                'job': 'Bartender'
-            }
-        ];
-        return (
-            <div className="container">
-                <Table characterData = { characters }/>
-            </div>
-        )
-    }
+
+const App = props => {
+    return (
+        <h1>This is an H1</h1>
+    )
 }
 
 export default App;
